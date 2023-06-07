912cf3d0f5a6ba2c3c7b5bdff8a1a5140fa2fe79
Jay Van Wyk <jayvanwyk@Jays-Mac-Studio.local>
2023-06-07 14:44:37 +0100

    Lesson 27

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,11 @@
-blockchain = [1]
+blockchain = [[0]]
 
-def add_value():
-    blockchain.append([blockchain[-1], 5.3])
+def add_value(amount):
+    blockchain.append([blockchain[-1], amount])
     print(blockchain)
 
-add_value()
-add_value()
-add_value()
+add_value(1.4)
+add_value(2.2)
+add_value(3.7)
+add_value(4.1)
+add_value(5.3)
