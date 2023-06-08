6b17023f2fda9d906f551b38ceceecf3f27e5401
Jay Van Wyk <jayvanwyk@Jays-Mac-Studio.local>
2023-06-08 10:01:14 +0100

    Lesson 33

--- blockchain.py
+++ blockchain.py
@@ -6,10 +6,14 @@ def get_last_blockchain_value():
 
 def add_value(transaction_amount, last_transaction=get_last_blockchain_value()):
     blockchain.append([last_transaction, transaction_amount])
-    print(blockchain)
 
-add_value(1.4,[1])
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=1.34)
-add_value(3.7)
-add_value(4.1)
-add_value(5.3)
+
+def get_user_input():
+    return float(input("Your transaction amount please: "))
+
+
+add_value(get_user_input(), get_last_blockchain_value())
+add_value(get_user_input(), get_last_blockchain_value())
+add_value(get_user_input(), get_last_blockchain_value())
+
+print(blockchain)
