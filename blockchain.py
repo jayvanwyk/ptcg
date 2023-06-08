49b44b7519235f4ad194b6be5d349c719c2e1c7a
Jay Van Wyk <jayvanwyk@Jays-Mac-Studio.local>
2023-06-08 09:53:05 +0100

    Lesson 30

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,15 @@
-blockchain = [[0]]
+blockchain = [0]
 
 def get_last_blockchain_value():
     return blockchain[-1]
 
-def add_value(amount):
-    blockchain.append([get_last_blockchain_value(), amount])
+
+def add_value(transaction_amount, last_transaction=get_last_blockchain_value()):
+    blockchain.append([last_transaction, transaction_amount])
     print(blockchain)
 
-add_value(1.4)
-add_value(2.2)
+add_value(1.4,[1])
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=1.34)
 add_value(3.7)
 add_value(4.1)
 add_value(5.3)
